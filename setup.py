d8a99629885aa1d61a7c3e3f12cdae29086373c1
amruth pai <26.amruth@gmail.com>
2025-08-23 06:58:05 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='care_guide_AI',
+    version='0.1.0',
+    author='Amruth Pai Thukaram',
+    author_email='26.amruth@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
+
